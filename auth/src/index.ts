de34d0ffe68d584f809859afdf33b05cb00ccacc
ajose-malik <ajose.malik@gmail.com>
2022-09-14 08:48:41 -0400

    fix kubernetes deployment issue

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,4 +3,10 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => {
+	res.send('Hi there')
+})
+
 app.listen(3000, () => console.log('STARTED 3000'))
+
+// gettix.dev/api/users/currentuser
