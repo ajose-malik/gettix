2f47a971ecc45c6130097bca0a60a2e8a7f9a9e1
ajose-malik <ajose.malik@gmail.com>
2022-09-22 19:30:33 -0400

    implement validation response

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -13,6 +13,12 @@ router.post(
 			.withMessage('Password must be between 4 and 20 characters')
 	],
 	(req: Request, res: Response) => {
+		const errors = validationResult(req)
+
+		if (!errors.isEmpty()) {
+			return res.status(400).send(errors.array())
+		}
+    
 		const { email, password } = req.body
 	}
 )
