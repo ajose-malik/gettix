cca75ba7f486f0b3ff425e05b4d63803831a66f4
ajose-malik <ajose.malik@gmail.com>
2022-09-28 18:32:25 -0400

    implement jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
@@ -23,7 +24,6 @@ router.post(
 		}
 
 		const { email, password } = req.body
-
 		const existingUser = await User.findOne({ email })
 
 		if (existingUser) {
@@ -33,6 +33,19 @@ router.post(
 		const user = User.build({ email, password })
 		await user.save()
 
+		// Generate jwt
+		const userJwt = jwt.sign(
+			{
+				id: user.id,
+				email: user.email
+			},
+			'asdf'
+		)
+
+		// Store on session object
+		req.session = {
+			jwt: userJwt
+		}
 		res.status(201).send(user)
 	}
 )
