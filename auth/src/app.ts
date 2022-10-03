c46dcc513a8464b295dcd4b5d3aba54b2f7059f4
ajose-malik <ajose.malik@gmail.com>
2022-10-03 07:11:13 -0400

    install dev dependencies

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
-
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
