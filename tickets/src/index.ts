a7178b541861d84a013728eeed245f0d92bb323d
ajose-malik <ajose.malik@gmail.com>
2022-10-13 23:40:40 -0400

    implement tickets deployment config

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,17 +5,16 @@ const start = async () => {
 	if (!process.env.JWT_GETTIX_KEY) {
 		throw new Error('JWT_GETTIX_KEY must be defined')
 	}
-
 	if (!process.env.MONGO_URI) {
-	throw new Error('MONGO_URI must be defined')
-}
+		throw new Error('MONGO_URI must be defined')
+	}
 
-try {
-	await mongoose.connect(process.env.MONGO_URI)
-	console.log('STARTED DATABASE')
-} catch (err) {
-	console.error(err)
-}
+	try {
+		await mongoose.connect(process.env.MONGO_URI)
+		console.log('STARTED DATABASE')
+	} catch (err) {
+		console.error(err)
+	}
 	app.listen(3000, () => console.log('STARTED PORT 3000'))
 }
 
