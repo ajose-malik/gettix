33f96b11e6353b8714a2f26f466518649eca6588
ajose-malik <ajose.malik@gmail.com>
2022-10-16 12:41:13 -0400

    implement ticket model and add route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,23 +1,59 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { Ticket } from '../../../models/tickets'
 
 it('should listen to ticket route for post request', async () => {
 	const response = await request(app).post('/api/tickets').send({})
 	expect(response.status).not.toEqual(404)
 })
 
-it('should sadly deny access if user is signed in', async () => {
+it('should sadly deny access if user is not signed in', async () => {
 	const response = await request(app).post('/api/tickets').send({})
 	expect(response.status).toEqual(401)
 })
 
-it('should happily grant access if user is signed in', async () => {
-	const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({})
-	expect(response.status).toEqual(200)
+it('should return error with invalid title', async () => {
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({ title: '', price: 10 })
+		.expect(400)
+
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({ price: 10 })
+		.expect(400)
 })
 
-it('should return error with invalid title', async () => {})
+it('should return error with invalid price', async () => {
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({ title: 'hello', price: -10 })
+		.expect(400)
 
-it('should return error with invalid price', async () => {})
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({ title: 'hello' })
+		.expect(400)
+})
 
-it('should create ticket with valid inputs', async () => {})
+it('should create ticket with valid inputs', async () => {
+	let tickets = await Ticket.find({})
+	expect(tickets.length).toEqual(0)
+
+	const title = 'hi'
+
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({ title, price: 20 })
+		.expect(201)
+
+	tickets = await Ticket.find({})
+	expect(tickets.length).toEqual(1)
+	expect(tickets[0].price).toEqual(20)
+	expect(tickets[0].title).toEqual(title)
+})
